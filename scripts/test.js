b45e95654d2475872128a73ec3f763b7f1f09615
Loz <laurence.ure@gmail.com>
2023-06-22 15:43:03 +0100

    Pushing current targeting and validation for harvesting

--- scripts/test.js
+++ scripts/test.js
@@ -9,3 +9,28 @@ Hooks.on("ready", function()
 {
   console.log("This code runs once core initialization is ready and game data is available.");
 });
+
+Hooks.on("targetToken", function(user, token, targeted)
+{
+  //console.log(user);
+  console.log(token);
+  //console.log(token.document.actorData.flags);
+  //console.log(user.name + " Targeted " + token.document.name + ": " + targeted);
+
+  var isDead = false;
+  token.document.actorData.effects.forEach(element => {
+    if (element.label == "Dead")
+      isDead = true;
+  });
+
+  if(user.targets.size == 1 && (token.document.actorData.system.attributes.hp.value == 0 && isDead) && targeted)
+  {
+    console.log(user.name + " can harvest " + token.document.name)
+    // await token.document.actorData.setProperty('harvester', 'harvestable', true);
+  }
+  else
+  {
+    console.log(user.name + " cannot harvest " + token.document.name)
+    // await token.document.actorData.setFlag('harvester', 'harvestable', false);
+  }
+});
