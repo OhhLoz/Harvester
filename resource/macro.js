7f7ff2d6c7012e256552c190428e3043444bf788
Loz <laurence.ure@gmail.com>
2023-06-25 00:02:49 +0100

    Updated macro with some rudimentary module load checking

--- resource/macro.js
+++ resource/macro.js
@@ -1,4 +1,11 @@
-const api = game.modules.get("harvester").api;
-let controlledToken = canvas.tokens.controlled[0];
-let targetedToken = game.user.targets.first();
-api.validateHarvest(controlledToken, targetedToken);
+const harvester = game.modules.get('harvester');
+if (!harvester)
+{
+  console.log('harvester module not found');
+}
+else
+{
+    let controlledToken = canvas.tokens.controlled[0];
+    let targetedToken = game.user.targets.first();
+    harvester.api.validateHarvest(controlledToken, targetedToken);
+}
