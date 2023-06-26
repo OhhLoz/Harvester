77f12cb10c58f3ec39f736d39bc90c40cf6a4fe4
Loz <laurence.ure@gmail.com>
2023-06-26 11:52:12 +0100

    Generalised alot of code in preparation for loot action
    It currently runs fine and adds the effect with the same rules as the harvest action

--- scripts/module.js
+++ scripts/module.js
@@ -1,20 +1,22 @@
 import { registerSettings, getSettings, dragonIgnoreArr, sizeHashMap } from "./settings.js";
 
-var actionCompendium, harvestCompendium, harvestEffect, moduleSettings, socket;
+var actionCompendium, harvestCompendium, lootCompendium, harvestEffect, lootEffect, moduleSettings, socket;
 
 Hooks.on("init", function()
 {
   registerSettings();
   moduleSettings = getSettings();
-  console.log("harvester | Init() - Registered settings.");
+  actionCompendium = game.packs.get("harvester.harvest-action").getDocuments();
+  harvestCompendium = game.packs.get("harvester.harvest").getDocuments();
+  lootCompendium = game.packs.get("world.loot");
+  console.log("harvester | Init() - Registered settings & Fetched compendiums.");
 });
 
-Hooks.on("ready", async function()
+Hooks.on("ready", function()
 {
-  actionCompendium = await game.packs.get("harvester.harvest-action").getDocuments();
-  harvestCompendium = await game.packs.get("harvester.harvest").getDocuments();
-  harvestEffect = actionCompendium[0].effects.get("0plmpCQ8D2Ezc1Do");
-  console.log("harvester | ready() - Assigned public functions & Fetched compendiums");
+  //lootCompendium = await game.tables.get("j7meAnkqPH4qO2zv");
+  harvestEffect = actionCompendium[1].effects.get("0plmpCQ8D2Ezc1Do");
+  lootEffect = actionCompendium[0].effects.get("gyGqnCpM8pGggHtB");
   if (game.user?.isGM && !game.modules.get("socketlib")?.active)
     ui.notifications.error("socketlib must be installed & enabled for harvester to function correctly.", { permanent: true });
   if (moduleSettings.autoAddActionGroup != "None")
@@ -23,7 +25,7 @@ Hooks.on("ready", async function()
 
 Hooks.once("socketlib.ready", () => {
 	socket = globalThis.socketlib.registerModule("harvester");
-	socket.register("addHarvestEffect", addHarvestEffect);
+	socket.register("addEffect", addEffect);
 	socket.register("addItemToActor", addItemToActor);
   console.log("harvester | Registed socketlib functions");
 });
@@ -36,21 +38,27 @@ Hooks.on("createActor", (actor, data, options, id) =>
       return;
 
     socket.executeAsGM(addItemToActor, actor.id, actionCompendium[0].id, actionCompendium[0].pack);
+    socket.executeAsGM(addItemToActor, actor.id, actionCompendium[1].id, actionCompendium[1].pack);
   }
 })
 
 function addActionToActors()
 {
-  var hasAction = false;
+  var hasHarvest = false;
+  var hasLoot = false;
   game.actors.forEach(actor =>
   {
     if(moduleSettings.autoAddActionGroup == "PCOnly" && actor.type == "npc")
       return;
     actor.items.forEach(item =>{
       if(item.name === "Harvest" && item.system.source == "Harvester")
-        hasAction = true;
+        hasHarvest = true;
+      if(item.name === "Loot" && item.system.source == "Harvester")
+        hasLoot = true;
     })
-    if (!hasAction)
+    if (!hasHarvest)
+      socket.executeAsGM(addItemToActor, actor.id, actionCompendium[1].id, actionCompendium[1].pack);
+    if (!hasLoot)
       socket.executeAsGM(addItemToActor, actor.id, actionCompendium[0].id, actionCompendium[0].pack);
   })
   console.log("harvester | ready() - Added Harvest Action to All Actors specified in Settings");
@@ -58,30 +66,32 @@ function addActionToActors()
 
 Hooks.on('dnd5e.preUseItem', function(item, config, options)
 {
-  if (item.name != "Harvest" && item.system.source != "Harvester")
+  if (item.system.source != "Harvester")
     return;
   var controlToken = item.parent.getActiveTokens()[0];
-  if(!validateHarvest(controlToken, game.user.targets))
+  if(!validateAction(controlToken, game.user.targets, item.name))
     return false;
 
   item._source.system.description.value = `${item.name}ing ${game.user.targets.first().name}`
-  //item._source.system.formula = "1d20 + @skills."+  +".mod"
+  // item._source.system.formula = "1d20 + @skills.ath.bonus"
+  // item._source.system.actionType = "abil"
 
   // Add skill check instead of rolling later on, will likely need a map to replace the skills with ability check?
 })
 
 Hooks.on('dnd5e.useItem', function(item, config, options)
 {
-  if (item.name != "Harvest" && item.system.source != "Harvester")
+  if (item.system.source != "Harvester")
     return;
 
   var controlToken = item.parent.getActiveTokens()[0];
   var targetToken = game.user.targets.first();
-  handleHarvest(targetToken, controlToken);
+  handleAction(targetToken, controlToken, item.name);
 })
 
-function validateHarvest(controlToken, userTargets)
+function validateAction(controlToken, userTargets, actionName)
 {
+  console.log(lootCompendium)
   if (userTargets.size != 1)
   {
     ui.notifications.warn("Please target only one token.");
@@ -92,7 +102,7 @@ function validateHarvest(controlToken, userTargets)
   var targetSize = sizeHashMap.get(targetedToken.actor.system.traits.size)
   if(measuredDistance > targetSize && moduleSettings.enforceRange)
   {
-    ui.notifications.warn("You must be in range to Harvest.");
+    ui.notifications.warn("You must be in range to " + actionName);
     return false;
   }
   if(targetedToken.document.actorData.system.attributes.hp.value != 0)
@@ -110,9 +120,9 @@ function validateHarvest(controlToken, userTargets)
     ui.notifications.warn(targetedToken.name + " is not an NPC");
     return false;
   }
-  if(checkEffect(targetedToken, "Harvested"))
+  if(checkEffect(targetedToken, `${actionName}ed`))
   {
-    ui.notifications.warn(targetedToken.name + " has been harvested already");
+    ui.notifications.warn(`${targetedToken.name} has been ${actionName.toLowerCase()}ed already`);
     return false;
   }
   return true;
@@ -129,9 +139,9 @@ function checkEffect(token, effectName)
   return returnBool;
 }
 
-function searchCompendium(actor)
+function searchCompendium(actor, actionName)
 {
-  var harvestArr = [];
+  var returnArr = [];
   var actorName = actor.name;
   if (actorName.includes("Dragon"))
     actorName = formatDragon(actorName);
@@ -139,9 +149,9 @@ function searchCompendium(actor)
   harvestCompendium.forEach(doc =>
   {
     if (doc.system.source === actorName)
-      harvestArr.push(doc);
+      returnArr.push(doc);
   })
-  return harvestArr;
+  return returnArr;
 }
 
 function formatDragon(actorName)
@@ -155,11 +165,14 @@ function formatDragon(actorName)
   return actorSplit;
 }
 
-function addHarvestEffect(targetTokenId)
+function addEffect(targetTokenId, actionName)
 {
   var targetToken = canvas.tokens.get(targetTokenId)
-  targetToken.toggleEffect(harvestEffect);
-  console.log(`harvester | Added harvest effect to: ${targetToken.name}`);
+  if(actionName == "Harvest")
+    targetToken.toggleEffect(harvestEffect);
+  else
+    targetToken.toggleEffect(lootEffect);
+  console.log(`harvester | Added ${actionName.toLowerCase()}ed effect to: ${targetToken.name}`);
 }
 
 function addItemToActor(actorId, itemId, packId)
@@ -170,45 +183,50 @@ function addItemToActor(actorId, itemId, packId)
   console.log(`harvester | Added item: ${item.name} to ${actor.name}`);
 }
 
-async function handleHarvest(targetedToken, controlledToken)
+async function handleAction(targetedToken, controlledToken, actionName)
 {
   var targetActor = await game.actors.get(targetedToken.document.actorId);
   var controlActor = await game.actors.get(controlledToken.document.actorId);
 
-  var itemArr = await searchCompendium(targetActor);
-  if (itemArr.length == 0)
+  var itemArr = [];
+  var result;
+  if (actionName == "Harvest")
   {
-    ChatMessage.create({content: "<h3>Harvesting</h3>After examining the corpse you realise there is nothing you can harvest."});
-    await socket.executeAsGM(addHarvestEffect, targetedToken.id);
-    return;
-  }
-
-  var skillCheck = itemArr[0]?.system.description.unidentified.slice(0,3).toLowerCase();
-  var result = await controlActor.rollSkill(skillCheck);
-  if (result)
-  {
-    var lootMessage = "";
-    var messageData = {content: {}, whisper: {}};
-    if (moduleSettings.gmOnly)
-      messageData.whisper = game.users.filter(u => u.isGM).map(u => u._id);
-
-    await socket.executeAsGM(addHarvestEffect, targetedToken.id);
-
-    itemArr.forEach(item =>
+    itemArr = await searchCompendium(targetActor, actionName);
+    if (itemArr.length == 0)
     {
-      if (parseInt(item.system.description.chat) <= result.total)
-      {
-        lootMessage += `<li>@UUID[${item.uuid}]</li>`
+      ChatMessage.create({content: `<h3>${actionName}ing</h3>After examining the corpse you realise there is nothing you can ${actionName.toLowerCase()}.`});
+      await socket.executeAsGM(addEffect, targetedToken.id, actionName);
+      return;
+    }
 
-        if(moduleSettings.autoAddItems)
-          socket.executeAsGM(addItemToActor, controlActor.id, item.id, item.pack);
-      }
-    });
-
-    if (lootMessage)
-      messageData.content = `<h3>Harvesting</h3><ul>${lootMessage}</ul>`;
-    else
-      messageData.content = `<h3>Harvesting</h3><ul>${controlledToken.name} attempted to harvest resources from ${targetedToken.name} but failed to find anything.`
-    ChatMessage.create(messageData);
+    var skillCheck = itemArr[0]?.system.description.unidentified.slice(0,3).toLowerCase();
+    result = await controlActor.rollSkill(skillCheck, {chooseModifier: false});
+    if (!result)
+      return;
   }
+
+  var lootMessage = "";
+  var messageData = {content: {}, whisper: {}};
+  if (moduleSettings.gmOnly)
+    messageData.whisper = game.users.filter(u => u.isGM).map(u => u._id);
+
+  await socket.executeAsGM(addEffect, targetedToken.id, actionName);
+
+  itemArr.forEach(item =>
+  {
+    if (parseInt(item.system.description.chat) <= result.total)
+    {
+      lootMessage += `<li>@UUID[${item.uuid}]</li>`
+
+      if(moduleSettings.autoAddItems)
+        socket.executeAsGM(addItemToActor, controlActor.id, item.id, item.pack);
+    }
+  });
+
+  if (lootMessage)
+    messageData.content = `<h3>${actionName}ing</h3><ul>${lootMessage}</ul>`;
+  else
+    messageData.content = `<h3>${actionName}ing</h3><ul>${controlledToken.name} attempted to ${actionName.toLowerCase()} resources from ${targetedToken.name} but failed to find anything.`
+  ChatMessage.create(messageData);
 }
